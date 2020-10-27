0429cf0aa4c1f767932c6d1aea2d9a05a9533dfe
pviniciusm <pvinics@gmail.com>
2020-10-27 10:45:14 -0300

    Identifier for exceptions

--- api/src/util/messages/Exception.ts
+++ api/src/util/messages/Exception.ts
@@ -1,14 +1,14 @@
 import Return from './Return';
 
 class Exception extends Return {
-  constructor (message: String, code?: Number) {
-    super(false, code || 500, message, undefined, true);
+  constructor (message: String, code?: Number, identifier?: String) {
+    super(false, code || 500, message, undefined, true, identifier);
   }
 }
 
 class RequiredFieldException extends Exception {
   constructor (field: String) {
-    super(field + ' is required.', 500);
+    super(field + ' is required.', 500, 'RequiredFieldException');
   }
 }
 
