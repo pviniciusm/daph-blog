6d21517be9184e824b2f9cb2b6b0077ecf989c99
pviniciusm <pvinics@gmail.com>
2020-12-14 22:58:22 -0300

    User routes url changes

--- api/src/routes/UserRouter.ts
+++ api/src/routes/UserRouter.ts
@@ -1,13 +1,14 @@
 import Router, { ParameterType } from './Router';
 import UserController from '../controllers/user/User';
+import { checkAuth } from '../util/middlewares/checkAuth';
 
 class UserRouter extends Router {
   constructor () {
     super(new UserController());
 
     // GET
-    this.get('/u/:email', UserController.prototype.get, ParameterType.Params);
-    this.get('/user', UserController.prototype.get, ParameterType.Query);
+    this.get('/:email', UserController.prototype.get, ParameterType.Params, [checkAuth]);
+    this.get('/:email/delete', UserController.prototype.remove, ParameterType.Params);
   }
 }
 
