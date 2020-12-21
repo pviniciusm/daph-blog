558505103acc6db5bbaa51f1cca7cf8a279d8b0b
pviniciusm <pvinics@gmail.com>
2020-12-20 23:26:59 -0300

    Add title field to Post entity

--- api/src/database/entities/Post.ts
+++ api/src/database/entities/Post.ts
@@ -16,6 +16,12 @@ export default class Post {
 
   @Column({
     type: 'varchar',
+    length: 60
+  })
+  title: string;
+
+  @Column({
+    type: 'varchar',
     length: 300
   })
   content: string;
